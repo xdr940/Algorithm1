d9cdc0e183d2d36ad355d968940a5e45639bc053
xdr940 <xdr94@126.com>
2018-11-29 16:23:04 +0800

    upd

--- BinTree/BinTree.cpp
+++ BinTree/BinTree.cpp
@@ -12,14 +12,14 @@ BinTree::~BinTree()
 {
 }
 
-void BinTree::Push(char c)
+void BinTree::Push(int c)
 {
 	set(this->root, c);
 }
 
 
 
-char BinTree::Back()
+int BinTree::Back()
 {
 	return 0;
 }
@@ -30,12 +30,12 @@ void BinTree::PreOrder()
 	preorder(this->root);
 }
 
-void BinTree::InOrder()const
+void BinTree::InOrder() 
 {
 	inorder(this->root);
 }
 
-void BinTree::preorder(BinNode *& t) const
+void BinTree::preorder(BinNode *& t)  
 {
 	if (t != nullptr) {
 		this->preorder(t->lchild);
@@ -44,7 +44,7 @@ void BinTree::preorder(BinNode *& t) const
 	}
 }
 
-void BinTree::inorder(BinNode*t) const
+void BinTree::inorder(BinNode*t)  
 {
 	if (t != nullptr) {
 		visit(t);
@@ -57,7 +57,7 @@ void BinTree::inorder(BinNode*t) const
 	}
 }
 
-void BinTree::levelorder(BinNode * t) const
+void BinTree::levelorder(BinNode * t)  
 {
 	queue<BinNode*>Q;
 	Q.push(t);
@@ -65,8 +65,15 @@ void BinTree::levelorder(BinNode * t) const
 		BinNode*p = Q.front();
 		visit(p);
 		Q.pop();
-		if (p->lchild != nullptr)Q.push(p->lchild);
-		if (p->rchild != nullptr)Q.push(p->rchild);
+		if (p) {
+			if (p->lchild != nullptr)Q.push(p->lchild);
+			else Q.push(nullptr);
+			if (p->rchild != nullptr)Q.push(p->rchild);
+			else Q.push(nullptr);
+
+		}
+		
+
 	}
 }
 
@@ -76,7 +83,7 @@ void BinTree::LevelOrder()
 }
 
 
-void BinTree::set(BinNode * &t, char c)
+void BinTree::set(BinNode * &t, int c)
 {
 	if (t == nullptr) {
 		t = new BinNode;//这个还是赋值，如果没有引用则赋值出set函数就失效
@@ -92,16 +99,66 @@ void BinTree::set(BinNode * &t, char c)
 
 
 
-void BinTree::visit(BinNode * t)const
+void BinTree::visit(BinNode * t)
 {
 	if (t != nullptr) {
 		cout << t->data << " ";
-
+		ret.push_back(t->data);
 	}
 	else {
 		cout << "# "; 
+		ret.push_back(Nan);
 	}
 
 
 }
+/*
+void BinTree::zigzag()
+{	
+	vector<vector<int>>xs;
+	vector<int>temp;
+	int cnt = 1;
+	int flag = 1;//从右往左
+	int sumcnt = 0;
+	while (cnt) {
+		temp.clear();
 
+		for (int j = sumcnt; j < sumcnt + cnt; j++) {
+			if (ret[j] != Nan) {
+				temp.push_back(ret[j]);
+			}
+
+		}
+
+		if (!temp.empty()) {
+			if (flag == 1) {
+				xs.push_back(temp);
+				flag = 2;
+			}
+			else {
+				reverse(temp.begin(), temp.end());
+				xs.push_back(temp);
+				flag = 1;
+			}
+		}
+		sumcnt += cnt;
+
+
+
+		cnt = 0;
+		for (int j = 0; j < temp.size(); j++) {
+			if (temp[j] != Nan)cnt++;
+		}
+		cnt *= 2;
+
+	}
+
+	//输出展示
+	for (int i = 0; i < xs.size(); i++) {
+		for (int j = 0; j < xs[i].size(); j++) {
+			cout << xs[i][j] << " ";
+		}cout << endl;
+	}
+}
+
+*/
--- BinTree/BinTree.h
+++ BinTree/BinTree.h
@@ -2,7 +2,7 @@
 
 
  struct BinNode{
-	 char data;
+	 int data;
 	 BinNode* lchild=nullptr, *rchild=nullptr;
 };
 class BinTree
@@ -10,23 +10,30 @@ class BinTree
 public:
 	BinTree();
 	~BinTree();
-	void Push(char c);
-	char Back();
+	void Push(int c);
+	int Back();
 	void PreOrder();
-	void InOrder()const;
+	void InOrder() ;
 	void LevelOrder();
-	
+	void disp() {
+		cout << endl;
+		for (int i = 0; i < ret.size(); i++) {
+			cout << ret[i] << " ";
+		}
+	}
+	//void zigzag();
+
 	BinNode*getRoot() { return this->root; }
 private:
-	void set(BinNode  *& t,char c);//注意这个函数的声明，要对t申请空间（赋值），所以加引用
-	void visit(BinNode*t)const;
+	void set(BinNode  *& t,int c);//注意这个函数的声明，要对t申请空间（赋值），所以加引用
+	void visit(BinNode*t);
 
-
-	void preorder(BinNode  *& t)const;
-	void inorder(BinNode*t)const;
-	void levelorder(BinNode*t)const;
+	void preorder(BinNode  *& t);
+	void inorder(BinNode*t);
+	void levelorder(BinNode*t);
 	BinNode* root;
 	int num=0;
+	vector<int>ret;
 	
 };
 
